aaae041f76f93d0cf77366aad9f27cdd67880943
Francisco <fsousa.jpa@gmail.com>
2022-10-17 16:26:42 -0300

    add funcoes matematicas e exercicio002

---
+++ src/main/kotlin/desafios/Exercicio002.kt
@@ -0,0 +1,31 @@
+package desafios
+
+fun main() {
+
+    conversaoAnos(2)
+    quantidadeCaracteres("Francisco")
+    calcularCubo(2)
+    converterMilhaKm(10.0)
+    trocaLetras("frAncisco")
+}
+
+fun conversaoAnos(anos: Int) {
+    println("$anos anos equivalem a:")
+    println("${anos * 12} meses")
+    println("${anos * 365} dias")
+    println("${anos * 365 * 24} horas")
+    println("${anos * 365 * 24 * 60} minutos")
+    println("${anos * 365 * 24 * 60 * 60} segundos")
+    println("-----------------------------------")
+}
+
+fun quantidadeCaracteres(str: String) = println("Quantidade de Caracteres: ${str.length}")
+
+fun calcularCubo(n: Int) = println("Cubo do N: ${n * n * n}")
+
+fun converterMilhaKm(milha: Double) = println("Quantidade em Km: ${milha * 1.6}")
+
+fun trocaLetras(str: String) {
+    println(str.lowercase().replace("a", "x", ignoreCase = true))
+}
+
---
+++ src/main/kotlin/funcoes/FuncoesMatematicas.kt
@@ -0,0 +1,13 @@
+package funcoes
+
+import kotlin.math.*
+
+fun main() {
+
+    println(max(5, 10)) //mostra valor mais alto entre 2 valores
+    println(min(5, 10)) //mostra valor mais baixo entre 2 valores
+    println(sqrt(25.0f)) // raiz quadrada
+    println(PI)
+    println(E) // numero de Euler
+    println(round(3456.4636435636))
+}
